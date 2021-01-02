3ec4e7d9e9e63179be2802fe8f158ad0ce21f9ea
Abin Joseph <itsmeabjs@gmail.com>
2021-01-02 19:20:02 +0530

    App is workig

--- ecom/api/migrations/0001_initial.py
+++ ecom/api/migrations/0001_initial.py
@@ -1,15 +1,16 @@
+# mv 0001_initial.py api/migrations
 from django.db import migrations
 from api.user.models import CustomUser
 
 
 class Migration(migrations.Migration):
     def seed_data(apps, schema_editor):
-        user = CustomUser(name="admin_user",
-                          email="admin_user@gmail.com",
+        user = CustomUser(name="abjs" ,#admin user name
+                          email="itsmeabjs@gmail.com",#Enter The Email
                           is_staff=True,
                           is_superuser=True,
                           )
-        user.set_password("password")
+        user.set_password("password") #Enter Your Admin Password
         user.save()
 
     dependencies = [
