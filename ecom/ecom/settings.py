c2cac0b04c5d09169410fb79d429be3d6613737b
Abin Joseph <itsmeabjs@gmail.com>
2021-01-02 18:40:52 +0530

    Add in settings

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
-    # 'api',
+    'api',
     # 'api.category',
     # 'api.product',
     # 'api.user',
